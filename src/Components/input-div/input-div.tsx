eb852ea4bcce73245f46d2191ee422302d778c2c
Ashish <ashishtaduce2@gmail.com>
2020-07-04 11:10:20 +0530

    section made

---
+++ src/Components/input-div/input-div.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./_input-div.scss"
+
+type Props = {};
+
+export default function InputDiv({}: Props) {
+    return (
+        <div className="input-div">
+            <textarea name="" id="" className={"input-area"} />
+
+        </div>
+    );
+}
